018cf7f605090ee0091555f417fab3701f223ac7
ihor_debelyi <s15293@pjwstk.edu.pl>
2020-12-07 16:56:17 +0100

    task 8 updated

--- task8/INL Project/task8.py
+++ task8/INL Project/task8.py
@@ -1,38 +1,36 @@
-import riak
+from riak import RiakClient
 
-def add_product(p_key, p_data):
-	product = my_bucket.new(p_key, data=p_data)
-	product.store()
 
 def fetch_and_print(p_key):
 	fetch_p = my_bucket.get(p_key)
+	if not fetch_p.exists:
+		print('Key does not exist in the database')
+		return
 	print(fetch_p.encoded_data)
 	return fetch_p
+
 
 bucket_name = 's_15293'
 ssd_key = 'ssd'
 
-my_client = riak.RiakClient(protocol='http', host='127.0.0.1', http_port=8098)
+my_client = RiakClient(protocol='http', host='localhost', http_port=8098)
 my_bucket = my_client.bucket(bucket_name)
-#add product to db
-ssd_data = {"name": "970 EVO Plus", "producer":"Samsung", "price":350, "capacityGb": 500}
-add_product(ssd_key, ssd_data)
-#retrieve product
+# add product to db
+ssd_data = {"name": "970 EVO Plus", "producer": "Samsung", "price": 350, "capacityGb": 500}
+ssd = my_bucket.new(ssd_key, data=ssd_data)
+ssd.store()
+print('added product')
+# add_product(ssd_key, ssd_data)
+# retrieve product
 ssd = fetch_and_print(ssd_key)
-#modify product
+# modify product
 ssd.data["name"] = "Samsung 970 EVO Plus updated"
 ssd.store()
-#retrieve updated
+print('updated')
+# retrieve updated
 ssd = fetch_and_print(ssd_key)
-#delete
+# delete
 ssd.delete()
-#try to retrieve deleted
-ssd = fetch_and_print(ssd)
-
-
-
-
-
-
-
-
+print('deleted')
+# try to retrieve deleted
+ssd = fetch_and_print(ssd_key)
